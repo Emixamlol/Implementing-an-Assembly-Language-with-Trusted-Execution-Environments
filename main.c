8a3f91d32b67c67b161f817889dc0112d4fb28bc
maxime <maxime.van.uffelen@vub.be>
2021-02-13 20:07:16 +0100

    separate file for main function ; might change

---
+++ main.c
@@ -0,0 +1,107 @@
+#include "sail.h"
+#include "rts.h"
+#include "elf.h"
+
+// union option
+enum kind_zoption { Kind_zNone };
+
+struct zoption {
+  enum kind_zoption kind;
+  union {struct { unit zNone; };};
+};
+
+static void CREATE(zoption)(struct zoption *op)
+{
+  op->kind = Kind_zNone;
+  
+}
+
+static void RECREATE(zoption)(struct zoption *op) {}
+
+static void KILL(zoption)(struct zoption *op)
+{
+  if (op->kind == Kind_zNone){
+    /* do nothing */
+  };
+}
+
+static void COPY(zoption)(struct zoption *rop, struct zoption op)
+{
+  if (rop->kind == Kind_zNone){
+    /* do nothing */
+  };
+  rop->kind = op.kind;
+  if (op.kind == Kind_zNone){
+    rop->zNone = op.zNone;
+  }
+}
+
+static bool EQUAL(zoption)(struct zoption op1, struct zoption op2) {
+  if (op1.kind == Kind_zNone && op2.kind == Kind_zNone) {
+    return EQUAL(unit)(op1.zNone, op2.zNone);
+  } else return false;
+}
+
+static void zNone(struct zoption *rop, unit op)
+{
+  if (rop->kind == Kind_zNone){
+    /* do nothing */
+  }
+  rop->kind = Kind_zNone;
+  rop->zNone = op;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+unit zmain(unit);
+
+unit zmain(unit zgsz30)
+{
+  __label__ end_function_1, end_block_exception_2;
+
+  unit zcbz30;
+  zcbz30 = print_endline("Hello World");
+end_function_1: ;
+  return zcbz30;
+end_block_exception_2: ;
+
+  return UNIT;
+}
+
+void model_init(void)
+{
+  setup_rts();
+}
+
+void model_fini(void)
+{
+  cleanup_rts();
+}
+
+int model_main(int argc, char *argv[])
+{
+  model_init();
+  if (process_arguments(argc, argv)) exit(EXIT_FAILURE);
+  zmain(UNIT);
+  model_fini();
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+  return model_main(argc, argv);
+}
