c3b8811ad47036565ff594ecf2c97a1bc469f5de
maxime <maxime.van.uffelen@vub.be>
2021-03-09 13:41:06 +0100

    created first test risc-v asm file

--- tests/hello.s
+++ tests/hello.s
@@ -1,7 +1,7 @@
 # https://www.youtube.com/watch?v=ODn7vnWOptM
 
     .text               # put this into the text section
-    .align  2           #align to a multiple of 4
+    .align  2           # align to a multiple of 4
     .globl  _start
 
 _start:
